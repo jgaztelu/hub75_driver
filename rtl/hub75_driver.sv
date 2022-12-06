f28b3669a40139bf884ccfb2c9ce364294f38ee7
jgaztelu <jgazte@gmail.com>
2022-12-06 23:29:52 +0100

    Instantiate framebuf in hub75_driver

--- rtl/hub75_driver.sv
+++ rtl/hub75_driver.sv
@@ -1,22 +1,25 @@
-module  #(
-    parameter hpixel_p = 64,    // Display width in pixels
-    parameter vpixel_p = 64,    // Display height in pixels
-    parameter  = bpp_p = 8;     // Bits per pixel color channel
-) hub75_driver (
+module hub75_driver #(
+    parameter  hpixel_p     = 64,                   // Display width in pixels
+    parameter  vpixel_p     = 64,                   // Display height in pixels
+    parameter  bpp_p        = 8,                    // Bits per pixel color channel
+    localparam frame_size_p = 64 * 64,
+    localparam addr_width_p = $clog2(frame_size_p)
+) (
     // Clock and reset
     input logic clk,
     input logic rst_n,
 
     /* Frame buffer write interface */
-    input logic [3*bpp_p-1:0] framebuf_wr_data, // Pixel data packed as {R,G,B}
-    input logic               framebuf_wr_en,   // Write enable
+    input logic [addr_width_p-1:0] i_framebuf_wr_addr,  // Framebuf write address
+    input logic [     3*bpp_p-1:0] i_framebuf_wr_data,  // Pixel data packed as {R,G,B}
+    input logic                    i_framebuf_wr_en,    // Write enable
 
     /* HUB75 outputs */
     // Control signals
-    output logic CLK,
+    output logic O_CLK,
     output logic STB,
     output logic OE,
-    
+
     // Row select
     output logic A,
     output logic B,
@@ -31,5 +34,22 @@ module  #(
     output logic B1,
     output logic B2
 );
-    
+
+  hub75_framebuf #(
+      .hpixel_p(64),
+      .vpixel_p(64),
+      .bpp_p   (8)
+  ) hub75_framebuf_i (
+      .clk(clk),
+      .rst_n(rst_n),
+      /* Write interface */
+      .i_wr_addr(i_framebuf_wr_addr),
+      .i_wr_data(i_framebuf_wr_data),
+      .i_wr_en(i_framebuf_wr_en),
+
+      /* Pixel read interface */
+      .i_rd_addr('0),
+      .o_rd_data()
+  );
+
 endmodule
--- rtl/hub75_framebuf.sv
+++ rtl/hub75_framebuf.sv
@@ -1,19 +1,31 @@
-module  #(
+module hub75_framebuf #(
     parameter hpixel_p = 64,    // Display width in pixels
     parameter vpixel_p = 64,    // Display height in pixels
-    parameter    bpp_p = 8;     // Bits per pixel color channel
-) hub75_framebuf (
+    parameter    bpp_p = 8,     // Bits per pixel color channel
+    localparam frame_size_p = 64*64,
+    localparam addr_width_p = $clog2(frame_size_p)
+) (
     // Clock and reset
     input logic clk,
     input logic rst_n,
 
     /* Write interface */
-    input logic             i_wr_en,
-    input logic [3*bpp-1:0] i_wr_data,
+    input logic [addr_width_p-1:0] i_wr_addr,
+    input logic [     3*bpp_p-1:0] i_wr_data,
+    input logic                    i_wr_en,
 
     /* Pixel read interface */
-    input logic                 i_rd_en,
-    output logic [2:0][bpp-1:0] o_rd_data
+    input logic [addr_width_p-1:0] i_rd_addr,
+    output logic [2:0][bpp_p-1:0] o_rd_data
 );
+
+  logic [frame_size_p-1:0][3*bpp_p-1:0] frame_buf;
+
+  always_ff @(posedge clk) begin
+    if (i_wr_en) frame_buf[i_wr_addr] <= i_wr_data;
+
+    o_rd_data <= frame_buf[i_rd_addr];
+  end
+
 
 endmodule
