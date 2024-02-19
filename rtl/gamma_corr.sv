3f06198a693b41c78ba0644d32b33eb0257c62c3
jgaztelu <jgazte@gmail.com>
2024-02-20 00:51:32 +0100

    Import existing gamma correction block

---
+++ rtl/gamma_corr.sv
@@ -0,0 +1,19 @@
+import gamma_corr_pkg::*;
+
+module gamma_corr #(
+  parameter pixel_width_p = 8
+  )
+  (
+    input logic clk,
+    input logic [pixel_width_p-1:0] pixel_in,
+    output logic [pixel_width_p-1:0] pixel_out
+  );
+
+  logic [7:0] gamma_pixel;
+
+  always_ff @(posedge clk) begin
+    pixel_out <= gamma_corr_c[pixel_in];
+    // pixel_out <= gamma_pixel;
+  end
+
+endmodule // gamma_corr
